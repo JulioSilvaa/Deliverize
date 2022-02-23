348791074ae371864261ac0faa9ccf003fd8635f
juliosan-bit <julioesar.amancio@yahoo.com.br>
2022-02-23 19:11:05 -0300

    Construindo um MVP para trabalhar com as dados

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { HomePage } from "./Pages/Home/HomePage";
+import './Pages/Styles/GlobalStyle.css'
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
